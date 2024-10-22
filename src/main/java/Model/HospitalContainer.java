b11888ee80df180672f2bd0b7b6866bfa59f92d6
MLuciaPerez <luu0808perez@gmail.com>
2024-10-22 01:18:05 -0300

    HospitalContainer class:   This class serves as a wrapper for the Hospital object, allowing for easier serialization and deserialization of the Hospital data structure to and from JSON.
    JSONParser class:
    This class handles the parsing of JSON files into Java objects and vice versa.
    
    Updated main application logic to include:
    Creation of sample Hospital data, including Departments and Doctors.
    Serialization of a HospitalContainer instance to JSON and saving it to a file, with logging of the serialized JSON.
    Deserialization of the JSON file back into a HospitalContainer object, followed by logging of the deserialized Hospital.
    Included a separate read method to demonstrate reading the JSON directly into a HospitalContainer.

---
+++ src/main/java/Model/HospitalContainer.java
@@ -0,0 +1,16 @@
+package Model;
+
+import Model.classesHierarchy.Hospital;
+
+public class HospitalContainer {
+    private Hospital hospital;
+
+    // Getters y setters
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
+}
